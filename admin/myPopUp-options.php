dc4b2513e8239b5c1eb5983d1614bea1871de119
unknown <karl.mullr@gmail.com>
2023-05-19 23:22:20 +0200

    le head

--- admin/myPopUp-options.php
+++ admin/myPopUp-options.php
@@ -5,11 +5,7 @@ function myPopUp_options_page_html()
     <div class="wrap">
         <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
         <form action="options.php" method="post">
-<<<<<<< HEAD
             
-=======
-            Veuillez choisir vos options
->>>>>>> 155788b164968218ca1e15e541c49762cbc812aa
             <?php
             // output security fields for the registered setting "myPopUp_options"
             settings_fields('myPopUp_options');
