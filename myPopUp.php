40172557ed8d26cb4ba4cf26752624250bf9954e
Franck Vienot <68828345+antikkorps@users.noreply.github.com>
2023-05-19 09:41:56 +0200

    Initiate the main php file of the plugin

---
+++ myPopUp.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:     My PopUp
+ * Version:         1
+ * Plugin URI:      
+ * Description:     Build graphical popups using your own forms
+ * Author:          Karl Muller  && Franck Vienot
+ * Author URI:      
+ * License:         GPL2
+ * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:     myPopUp
+ * Domain Path:     /languages
+ *
+ * @package         My_PopUp
+ */
