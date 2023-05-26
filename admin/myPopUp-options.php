182a4bbcda09e30f1f24cf6daf13e3a4712d3226
Franck <franckvienot7@gmail.com>
2023-05-26 20:17:17 +0200

    add navigation menu for options

--- admin/myPopUp-options.php
+++ admin/myPopUp-options.php
@@ -3,11 +3,21 @@ function myPopUp_options_page_html()
 {
 ?>
     <div class="wrap">
-        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+        <h1 class="mb-5"><?php echo esc_html(get_admin_page_title()); ?></h1>
+
+        <div class="tabs is-centered is-box is-medium mt-6">
+            <ul>
+                <li class="is-active"><a>Activation</a></li>
+                <li><a>Visuel</a></li>
+                <li><a>conditions</a></li>
+                <li><a>Options supplémentaires</a></li>
+            </ul>
+        </div>
+
         <div class="container is-fluid">
 
-        <input type="text" class="custom-field" name="custom-field" placeholder="Custom field">
-        <button type="submit" class="submit">Submit</button>
+            <input type="text" class="custom-field" name="custom-field" placeholder="Custom field">
+            <button type="submit" class="submit">Submit</button>
             <form class="my-5" action="options.php" method="post">
 
                 <div class="field">
