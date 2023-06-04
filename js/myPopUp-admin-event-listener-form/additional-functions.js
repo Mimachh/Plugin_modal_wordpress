6d3363d016946602c0010664ff03dacebe8a5ef8
Franck <franckvienot7@gmail.com>
2023-06-04 15:41:25 +0200

    add change in style in error message

--- js/myPopUp-admin-event-listener-form/additional-functions.js
+++ js/myPopUp-admin-event-listener-form/additional-functions.js
@@ -1,51 +1,48 @@
-
-
-function mpuValidateTitle(mpuTitle) { 
-    if (mpuTitle.value.trim() === '') {
-      errorMessage.textContent = 'Le titre est obligatoire';
-      errorMessage.style.display = 'block';
-      return false; // La validation a échoué
-    } 
-    return true;
+function mpuValidateTitle(mpuTitle) {
+  if (mpuTitle.value.trim() === '') {
+    errorMessage.textContent = 'Le titre est obligatoire';
+    errorMessage.style.display = 'block notification is-danger';
+    return false; // La validation a échoué
+  }
+  return true;
 }
 
 // Fonction pour cacher un groupe de div
 function hideElementGroup(element) {
-    element.style.display = 'none';
-    clearCheckboxInputs(element);
+  element.style.display = 'none';
+  clearCheckboxInputs(element);
 }
 
 function hideImageButtonAndFieldGroup(element, imgPreview) {
-    element.style.display = 'none';
-    if(imgPreview) {
-        clearImagePreview(imgPreview);
-    }
+  element.style.display = 'none';
+  if (imgPreview) {
+    clearImagePreview(imgPreview);
+  }
 }
-        
+
 // Fonction pour afficher un groupe de div
 function showElementGroup(element) {
-    element.style.display = 'block';
+  element.style.display = 'block';
 }
 
 function clearCheckboxInputs(element) {
-    const checkboxes = element.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach(function(checkbox) {
-        checkbox.checked = false;
-    });
+  const checkboxes = element.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(function (checkbox) {
+    checkbox.checked = false;
+  });
 }
 
 function clearImagePreview(element) {
-    const imagePreview = document.querySelector(element);
-    console.log(element);
-    imagePreview.src = ''; // Efface l'URL de l'image
-    imagePreview.alt = 'Aperçu de l\'imagsssssse'; // Rétablit le texte alternatif de l'image
-    
-    imagePreview.removeEventListener('error', errorHandler);
+  const imagePreview = document.querySelector(element);
+  console.log(element);
+  imagePreview.src = ''; // Efface l'URL de l'image
+  imagePreview.alt = "Aperçu de l'imagsssssse"; // Rétablit le texte alternatif de l'image
+
+  imagePreview.removeEventListener('error', errorHandler);
 }
 
 function errorHandler() {
-    const imagePreview = this;
-    imagePreview.src = '';
-    imagePreview.alt = 'Erreur de chargement de l\'image';
+  const imagePreview = this;
+  imagePreview.src = '';
+  imagePreview.alt = "Erreur de chargement de l'image";
 }
-
