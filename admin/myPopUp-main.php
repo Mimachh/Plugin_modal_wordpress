97115971fb84aed0257a12228e8094452239d58a
Franck <franckvienot7@gmail.com>
2023-06-04 17:42:45 +0200

    added some changes on main page and styles

--- admin/myPopUp-main.php
+++ admin/myPopUp-main.php
@@ -5,7 +5,7 @@ function myPopUp_main_page_html()
     require_once dirname(dirname(__FILE__)) . '/admin/partials/my-popup-navbar.php';
 ?>
 
-    <div class="mpu-wrapper" style="height: 100%">
+    <div class="mpu-wrapper">
         <?php myPopUp_navbar(); ?>
         <div class="wrap">
 
@@ -14,13 +14,16 @@ function myPopUp_main_page_html()
             <div class="notification mt-5">
                 <p>Ce plugin vous permet de créer et afficher des pop-ups sur votre site WordPress.</p>
             </div>
-            <div class="notification is-warning">
-                <p><strong>Note :</strong> myPopUp propose une version gratuite avec des fonctionnalités de base. Pour accéder à des fonctionnalités avancées telles que des modèles de pop-ups prédéfinis, des intégrations avec des services de marketing et une assistance premium, envisagez de passer à la version payante.</p>
-                <p class="mb-5">Visitez notre site web pour en savoir plus sur les fonctionnalités de la version payante et pour obtenir votre licence.</p>
-                <a href="https://www.mon-site.com/my-popup-pro" class="button is-primary" target="_blank">Découvrir la version payante</a>
+            <div class="notification-container">
+                <div class="notification is-warning">
+                    <p class="is-size-4"><strong>Note : myPopUp est un plugin gratuit avec des fonctionnalités de base.</strong></p>
+                    <p class="my-5">Pour accéder à des fonctionnalités avancées telles que des modèles de pop-ups prédéfinis, des intégrations avec des services de marketing et une assistance premium, vous pouvez envisager de passer à la version payante.</p>
+                    <p class="mb-5">Visitez notre site web pour en savoir plus sur les fonctionnalités de la version payante et pour obtenir votre licence.</p>
+                    <a href="https://www.mon-site.com/my-popup-pro" class="button is-primary" target="_blank">Découvrir la version payante</a>
+                </div>
             </div>
             <div class="notification">
-                <p>Merci d'utiliser ce plugin pour la création de vos modals.</p>
+                <p>Merci d'utiliser ce plugin pour la création de vos Pop-Ups.</p>
                 <p>Commencez par configurer les options du plugin en cliquant sur le lien ci-dessous.</p>
 
                 <a href=<?php echo admin_url('admin.php?page=myPopUp-options'); ?> class="button is-primary my-5" value="Redirection">Configurer les options</a>
